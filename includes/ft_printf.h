8d45d7d8049d35a3eab19d7d254ec748ffd1bb9f
Nasembele <50178818+Nasembele@users.noreply.github.com>
2020-03-01 23:11:40 +0300

    Add files via upload

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,104 +6,78 @@
 /*   By: soyster <soyster@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/30 01:18:14 by soyster           #+#    #+#             */
-/*   Updated: 2020/03/01 15:16:04 by mleticia         ###   ########.fr       */
+/*   Updated: 2020/02/19 19:49:59 by soyster          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-# include <stdarg.h>
-# include <stdio.h>
-# include <unistd.h>
-# include "../libft/libft.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "../libft/libft.h"
 
-typedef	struct	s_flag
+//#define ALL_FLAGS #0-+ 
+
+typedef struct	s_flar
 {
-	char		*format;
-	int			i;
-	int			res;
-	char		f_sh;
-	char		f_0;
-	char		f_min;
-	char		f_pl;
-	char		f_sp;
-	int			width;
-	int			check_prec;
-	int			prc;
-	int			len;
-	char		spc;
+	const char	*format; //строка формата
+
+	int			i; //index для строки формата
+	int			result; //кол-во напечатанных символов
+	
+	char		flag_sh; //#
+	char		flag_0;  //0=N видит 0 только если он сразу после %
+	char		flag_min; //-
+	char		flag_pl;  //+
+	char		flag_sp;  //space = S
+	
+	int			width; // ширина
+	int			precision; //точность
+	int			length; //длина 1 - h, 2 - hh, 3 - l, 4 - ll, 5 - L
+	char		specifier; //спецификатор
+	long double	f; // храниться флот
+	int			*fractional; //дробная часть флота
+	int			index; //индекс для дробной части флота
 }				t_flag;
 
-typedef struct	s_float
+typedef union			u_double //убрать
 {
-	long double	f;
-	long		n_st;
-	long double	n_ed;
-	int			*frt;
-	int			i;
-	int			k;
-	int			ch_s;
-	int			len_wh;
-}				t_float;
+   long double			ld;
+   struct
+   {
+      unsigned long int	mant : 64;
+      unsigned int		expon : 15;
+      unsigned int		sign : 1;
+   }					s_parts;
+}						t_double;
 
-void			ft_space(t_flag *all_mod, char *sign, int len);
-void			ft_zero(t_flag *all_mod);
-void			ft_sign(t_flag *all_mod, char *sign);
-int				base_put(t_flag *all_mod, long long num, int base);
-int				ft_sharp(t_flag *all_mod, int len);
-void			to_spec_id(t_flag *all_mod, int len);
-void			ft_negative(t_flag *all_mod, char *sign, int len);
-void			to_spec_plus(t_flag *all_mod, int len);
-void			ft_doublesign(t_flag *all_mod, char *sign, int len);
-void			ft_doublesignend(t_flag *all_mod, char *sign, int len);
-void			ft_digit(t_flag *all_mod, char *sign, long long num, int len);
-void			ft_spec_k(va_list all_arg, t_flag *all_mod);
-void			ft_color_2(t_flag *all_mod);
-void			ft_eoc(t_flag *all_mod);
-void			ft_color(t_flag *all_mod);
-int				ft_strncmp_st(const char *s1, int k, const char *s2, int n);
-int				ft_gen_parser(va_list all_arg, char *format);
-int				ft_c(int prc, char s);
-int				ft_wid_prec(t_flag *all_mod);
-void			ft_free_fl(t_float	*fl);
-void			ft_putlongnbr(long n, t_flag *all_mod);
-void			ft_print_fl(t_float *fl, t_flag *all_mod);
-int				ft_act_width(t_float *fl, t_flag *all_mod);
-int				ft_len_whole(long num_start);
-void			ft_print_sp(t_float *fl, t_flag *all_mod);
-void			ft_print_fl_min(t_float *fl, t_flag *all_mod);
-void			ft_check_s(t_float *fl, t_flag *all_mod);
-void			ft_fill_frt(t_float *fl, t_flag *all_mod);
-void			ft_separ_fl(t_float *fl, t_flag *all_mod);
-int				ft_float_1(long double f, int prc);
-void			ft_fill_fl(t_float *fl);
-int				ft_rounding(t_float *fl);
-void			ft_spec_f(va_list all_arg, t_flag *all_mod);
-void			ft_put_dec(t_flag *all_mod, long long num, int base);
-void			ft_spec_x(t_flag *all_mod, int len);
-int				ft_wx(int len, char c);
-int				ft_sharp(t_flag *all_mod, int len);
-void			ft_num(t_flag *all_mod, long long num, int base, int len);
-int				ft_spec_x_add(t_flag *all_mod, int len);
-void			ft_percent(t_flag *all_mod);
-int				ft_simple_width(int simple_width, t_flag *all_mod);
-int				base_put(t_flag *all_mod, long long num, int base);
-void			ft_putstr_len(char const *str, size_t len_prec,
-							  t_flag *all_mod);
-int				ft_spec_s(va_list all_arg, t_flag *all_mod);
-int				ft_spec_c(va_list all_arg, t_flag *all_mod);
-int				ft_width(t_flag *all_mod, int len);
-void			ft_free_allmod(t_flag *all_mod);
-int				ft_spec_p(t_flag *all_mod, long long num, int base, int len);
-void			ft_put_dec(t_flag *all_mod, long long num, int base);
-int				ft_length(va_list all_arg, t_flag *all_mod);
-int				ft_spec(va_list all_arg, t_flag *all_mod);
-int				ft_flags(va_list all_arg, t_flag *all_mod);
-int				ft_wid_and_prec(va_list all_arg, t_flag *all_mod);
-int				ft_printf(const char *format, ...);
-void			ft_x(t_flag *all_mod, long long num, int base, int len);
-size_t			ft_function(va_list all_arg, t_flag *all_mod);
-int				ft_gen_parser(va_list all_arg, char *format);
+typedef struct			s_mant_list //убрать
+{
+	unsigned long		base_list;
+	struct s_mant_list	*next;
+}						t_mant_list;
+
+int		ft_len_whole(long num_start);
+int		ft_rounding(t_flag *all_mod);
+void	ft_spec_f(va_list all_arg, t_flag *all_mod);
+int		ft_spec_x(t_flag *all_mod, char *pointer);
+int		ft_spec_p(t_flag *all_mod, char *pointer);
+void	ft_percent(t_flag *all_mod);
+int		ft_simple_width(int simple_width, int result);
+int		ft_width(t_flag *all_mod, int len);
+void	ft_putstr_len(char const *str, size_t len_prec);
+int		ft_spec_s(va_list all_arg, t_flag *all_mod);
+int		ft_spec_c(va_list all_arg, t_flag *all_mod);
+t_flag	*ft_free_allmod(t_flag *all_mod);
+int		ft_length(va_list all_arg, t_flag *all_mod);
+int		ft_spec(va_list all_arg, t_flag *all_mod);
+int		ft_flags(va_list all_arg, t_flag *all_mod);
+int		ft_wid_and_prec(va_list all_arg, t_flag *all_mod);
+int		ft_printf(const char *format, ...);
+int		ft_findfunction(va_list all_arg, t_flag *all_mod);
+int		*ft_itoa_base(va_list all_arg, t_flag *all_mod, int base);
+size_t	count_for_malloc(long long nbr, int base);
 
 #endif
