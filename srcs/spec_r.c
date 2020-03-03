1d36009803a76e38db0f9a641f0d52e4aba1cec5
Nasembele <50178818+Nasembele@users.noreply.github.com>
2020-03-03 11:09:39 +0300

    Add files via upload

--- srcs/spec_r.c
+++ srcs/spec_r.c
@@ -6,7 +6,7 @@
 /*   By: soyster <soyster@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/29 21:17:49 by soyster           #+#    #+#             */
-/*   Updated: 2020/03/01 20:23:33 by soyster          ###   ########.fr       */
+/*   Updated: 2020/03/03 11:01:54 by soyster          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ void	ft_np_1(t_np *np, t_flag *all_mod)
 {
 	while (np->src[np->i])
 	{
-		np->src[np->i] == 0 ? ft_putstr_res("[NUL]", 6, all_mod) : 0;
+		np->src[np->i] == 0 ? ft_putstr_res("[NUL]", 5, all_mod) : 0;
 		np->src[np->i] == 1 ? ft_putstr_res("[SOH]", 5, all_mod) : 0;
 		np->src[np->i] == 2 ? ft_putstr_res("[STX]", 5, all_mod) : 0;
 		np->src[np->i] == 3 ? ft_putstr_res("[ETX]", 5, all_mod) : 0;
@@ -39,7 +39,7 @@ void	ft_np_1(t_np *np, t_flag *all_mod)
 		np->src[np->i] == 5 ? ft_putstr_res("[ENQ]", 5, all_mod) : 0;
 		np->src[np->i] == 6 ? ft_putstr_res("[ACK]", 5, all_mod) : 0;
 		np->src[np->i] == 7 ? ft_putstr_res("[BEL]", 5, all_mod) : 0;
-		np->src[np->i] == 8 ? ft_putstr_res("[BS]", 5, all_mod) : 0;
+		np->src[np->i] == 8 ? ft_putstr_res("[BS]", 4, all_mod) : 0;
 		np->src[np->i] == 9 ? ft_putstr_res("[TAB]", 5, all_mod) : 0;
 		np->src[np->i] == 10 ? ft_putstr_res("[LF]", 4, all_mod) : 0;
 		np->src[np->i] == 11 ? ft_putstr_res("[VT]", 4, all_mod) : 0;
@@ -54,7 +54,7 @@ void	ft_np_1(t_np *np, t_flag *all_mod)
 
 void	ft_np_2(t_np *np, t_flag *all_mod)
 {
-	np->src[np->i] == 17 ? ft_putstr_res("[DC1]", 6, all_mod) : 0;
+	np->src[np->i] == 17 ? ft_putstr_res("[DC1]", 5, all_mod) : 0;
 	np->src[np->i] == 18 ? ft_putstr_res("[DC2]", 5, all_mod) : 0;
 	np->src[np->i] == 19 ? ft_putstr_res("[DC3]", 5, all_mod) : 0;
 	np->src[np->i] == 20 ? ft_putstr_res("[DC4]", 5, all_mod) : 0;
@@ -62,9 +62,9 @@ void	ft_np_2(t_np *np, t_flag *all_mod)
 	np->src[np->i] == 22 ? ft_putstr_res("[SYN]", 5, all_mod) : 0;
 	np->src[np->i] == 23 ? ft_putstr_res("[ETB]", 5, all_mod) : 0;
 	np->src[np->i] == 24 ? ft_putstr_res("[CAN]", 5, all_mod) : 0;
-	np->src[np->i] == 25 ? ft_putstr_res("[EM]", 5, all_mod) : 0;
+	np->src[np->i] == 25 ? ft_putstr_res("[EM]", 4, all_mod) : 0;
 	np->src[np->i] == 26 ? ft_putstr_res("[SUB]", 5, all_mod) : 0;
-	np->src[np->i] == 27 ? ft_putstr_res("[ESC]", 4, all_mod) : 0;
+	np->src[np->i] == 27 ? ft_putstr_res("[ESC]", 5, all_mod) : 0;
 	np->src[np->i] == 28 ? ft_putstr_res("[FS]", 4, all_mod) : 0;
 	np->src[np->i] == 29 ? ft_putstr_res("[GS]", 4, all_mod) : 0;
 	np->src[np->i] == 30 ? ft_putstr_res("[RS]", 4, all_mod) : 0;
